09f31872049bd76037015b7b71cbe7c7d153e82c
tube1008 <ilhyuk0504@gmail.com>
2023-12-27 18:13:18 +0900

    bomdom

---
+++ script/bom2.js
@@ -0,0 +1,44 @@
+/* let m0 = 'popup';
+let m1 = window.alert('popup');
+let m2 = Window.alert('잠시기다려주세요.');
+let m3 = alert('준비중입니다.');
+let m4 = window.alert('잘못접근하셨습니다.');
+let m5 = window.alert('제출 완료되었습니다.');
+const m6 = window.alert('결제 실패했습니다.');
+console.log(m1); */
+
+/* const qa1 = window.prompt('당신의 나이는?','');
+console.log(qa1)
+console.log(typeof qa1) */
+
+/* const ok = window.confirm('결제하시겠습니까?');
+console.log(ok); */
+
+// window.print();
+
+/* const naver = window.location.href='https://www.baver.com'
+console.log(naver); */
+
+function naver(){
+    window.location.href="https://naver.com"
+};
+
+//--------------------------------
+//객체.속성.속성
+//객체.속성.메서드()
+window.document.write("테스트 문구")
+document.write('자바스크립트')
+document.write('hellow<br>world')
+//--------------------------------------
+// window.open('https://google','_blank');
+function func2(){
+    window.open('https://google.com','_blank','width=500, height=300')
+    window.open('https://google.com','_blank','width=500, height=300')
+}
+
+
+
+/* 
+okky 프론트.벡엔드 개발자 커뮤니티 사이트
+
+하드코딩하는사람들 커뮤니티(네이버카페) */
