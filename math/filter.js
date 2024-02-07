863d4f644a370bd9a93c08c10fbdc3a375ec44b0
Ahn il hyuk <ilhyuk0504@gmail.com>
2024-02-07 11:06:21 +0900

    math 와 배열정렬

---
+++ math/filter.js
@@ -0,0 +1,15 @@
+const shopping_data = [
+    {
+        name:'원피스1',
+        price:2000,
+        color:'balck',
+    },{
+        name:'스커트',
+        price:5000,
+        color:'white',
+    },{
+        name:'원피스2',
+        price:3000,
+        color:'balck',
+    }
+]
